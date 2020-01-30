b80281d89350d2c13b1a7863f1fc1a4f4977d12c
Thomas Yi <thomasyi79@gmail.com>
2020-01-30 17:31:57 -0500

    created try and catch statements

--- ConditionalStatement/Program.cs
+++ ConditionalStatement/Program.cs
@@ -11,51 +11,48 @@ namespace ConditionalStatement
     {
         static void Main(string[] args)
         {
-            // Ask the user estimated grade input
-            Console.WriteLine("What grade do you think you will get in ISM 4300?");
+            // Ask the user for input
+            Console.WriteLine("Please enter your grade.");
 
-            /* use the try catch block to validate user input
-                if the user provides bad input, the catch block will
-                handle the error and a message will be displayed.
-            */
+            //use try and catch to prevent any value type other than Int.
             try
             {
-                // this variable will handle the input
+                //gather data from user
                 string input = Console.ReadLine();
-                // converting integer input to a character
+
+                // convert a string into an int value
                 int grade = int.Parse(input);
-                // if statement for to show the letter grade: A
+
+                // use if statement to limit value entered between 0-100, and display message
+                if (grade > 100 || grade < 0)
+                    Console.WriteLine("Value entered cannot be greater than 100 or smaller than 0. Please exit and try again");
+                
+                // If grade is between 90-100, display message
                 if (grade >= 90 && grade <= 100)
-                {
-                    Console.WriteLine("Your letter grade is: A.");
-                }
-                else if (grade >= 80 && grade <= 89)
-                {
-                    // if statement for to show the letter grade: B
-                    Console.WriteLine("Your letter grade is: B.");
-                }
-                else if (grade >= 70 && grade <= 79)
-                {
-                    // if statement for to show the letter grade: C
-                    Console.WriteLine("Your letter grade is: C.");
-                }
-                else if (grade >= 60 && grade <= 69)
-                {
-                    // if statement for to show the letter grade: D
-                    Console.WriteLine("Your letter grade is: D.");
-                }
-                else
-                {
-                    // if statement for to show the letter grade: F
-                    Console.WriteLine("Your letter grade is: F.");
-                }
-            } // end of try
+                    Console.WriteLine("Your letter grade is: A");
+                // If grade is between 80-90, display message
+                else if (grade >= 80 && grade < 90)
+                    Console.WriteLine("Your letter grade is: B");
+                // If grade is between 70-80, display message
+                else if (grade >= 70 && grade < 80)
+                    Console.WriteLine("Your letter grade is: C");
+                // If grade is between 60-70, display message
+                else if (grade >= 60 && grade < 70)
+                    // If grade below 60, display message
+                    Console.WriteLine("Your letter grade is: D");
+                else if (grade < 60 && grade >= 0)
+                    Console.WriteLine("Your letter grade is: F");
+
+
+            } // End of try
             catch
             {
-                Console.WriteLine("Please enter an integer to continue...");
-                Console.WriteLine("---OR---");
-                Console.WriteLine("Press any button to exit...");
-            } // end of catch 
-        } // end of string
-    }
+                //display messages when user enters any other data type other than int
+                Console.WriteLine("Please enter a int data type for your grade next time...");
+                Console.WriteLine("Press any key to exit the program and try again...");
+                // Pause the program and await the user to press a key to end the program
+                Console.ReadKey(true);
+            } // end of catch
+        }
+        }
 }
